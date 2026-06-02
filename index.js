0d7aed9b497cd82e3bdc5365cafd1377c4a4ca68
marasigamhoho-glitch <marasigamhoho@gmail.com>
2026-06-02 12:27:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Hosseasmp.aternos.me'
+    port: 15416,
+    username: 'Sigmabot',
+    version: '1.20.1',
+    auth: 'offline'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Drippy Bot is online! 🔥')
+
+    // Anti AFK - moves every 30 seconds
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+    }, 30000)
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log('Bot got kicked:', reason)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('end', () => {
+    console.log('Bot disconnected, reconnecting...')
+    setTimeout(createBot, 5000)
+  })
+}
+
+createBot()
+
